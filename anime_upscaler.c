4b72f5bd15e00f34248b2feb8572f9d15db4b3aa
theturboturnip <popgoestoast@gmail.com>
2018-11-04 13:18:53 +0000

    Implemented command-line options

--- anime_upscaler.c
+++ anime_upscaler.c
@@ -12,6 +12,7 @@
 #include <libgen.h>
 #include <stdatomic.h>
 #include <poll.h>
+#include <getopt.h>
 
 #include "expandable_buffer.h"
 #include "process_utils.h"
@@ -70,7 +71,7 @@ void cleanup(){
 }
 
 // Use waifu2x to upscale 16 images per round
-#define FRAMES_PER_UPSCALE_ROUND 256
+//#define FRAMES_PER_UPSCALE_ROUND 256
 
 void get_framerate(char* filepath, char* output, size_t output_length){
 	pipe_data ffmpeg_framerate_output_pipe = create_pipe_data();
@@ -84,26 +85,134 @@ void get_framerate(char* filepath, char* output, size_t output_length){
 	FILE* ffmpeg_framerate_output = fdopen(ffmpeg_framerate_output_pipe.files.read_from, "r");
 
 	fread(output, sizeof(char), output_length, ffmpeg_framerate_output);
+	// Remove trailing newline
+	output[strcspn(output, "\n")] = 0;
 	
 	fclose(ffmpeg_framerate_output);
 	pipe_data_close(&ffmpeg_framerate_output_pipe);
 	free(ffmpeg_framerate_command);
 }
 
-int main(int argc, char* argv[]){
-	if (argc < 3){
-		fprintf(stderr, "Usage: %s [input_file] [output_file]\n", argv[0]);
-		return 1;
+#define DEFAULT_FRAMES_PER_UPSCALE_ROUND 256
+
+static struct {
+	char* input_filepath;
+	char* output_filepath;
+	
+	size_t frames_per_upscale_round;
+
+	int should_mute_ffmpeg_source;
+	int should_mute_waifu2x;
+	int should_mute_ffmpeg_result;
+
+	char* waifu2x_folder;
+	char* waifu2x_file;
+	char* waifu2x_model;
+
+	int dry_run;
+} options = {
+	.input_filepath = NULL,
+	.output_filepath = NULL,
+	.frames_per_upscale_round = DEFAULT_FRAMES_PER_UPSCALE_ROUND,
+	.should_mute_ffmpeg_source = 1,
+	.should_mute_waifu2x = 0,
+	.should_mute_ffmpeg_result = 1,
+	.waifu2x_folder = "./waifu2x/",
+	.waifu2x_file = "waifu2x.lua",
+	.waifu2x_model = "models/photo",
+	.dry_run = 0
+};
+
+void print_help(FILE* file, int argc, char* argv[]){
+	fprintf(file, "Usage: %s [--frame-count=<FC>] [--waifu2x=<PATH>] [--waifu2x-model=<NAME>] [--] input-file output-file\n", argv[0]);
+	fprintf(file, "Options:\n\
+ -h --help        Show this screen\n\
+ --frame-count    Set the amount of frames that are concurrently processed. Default: %d\n\
+ --waifu2x        Set the folder containing waifu2x.lua. waifu2x will be executed from here. Default: ./waifu2x/\n\
+ --waifu2x-model  Set the model used by waifu2x to upscale images. Default: models/photo\n\
+ -d --dry-run     Do a dry run without running anything\n",
+			DEFAULT_FRAMES_PER_UPSCALE_ROUND);
+}
+void get_options(int argc, char* argv[]){
+	static char* short_options = "hd";
+	static struct option long_options[] = {
+		{ "frame-count", required_argument, NULL, 'f' },
+		{ "waifu2x", required_argument, NULL, 'w' },
+		{ "waifu2x-model", required_argument, NULL, 'm' },
+		{ "help", no_argument, NULL, 'h' },
+		{ "dry-run", no_argument, &options.dry_run, 1 }
+	};
+	int option_index = 0;
+
+	while(1){
+		char option_code = getopt_long(argc, argv, short_options, long_options, &option_index);
+		if (option_code == -1) break;
+		
+		switch(option_code){
+		case 'h':
+			print_help(stdout, argc, argv);
+			exit(0);
+		case 'f':
+			if (sscanf(optarg, "%zu", &options.frames_per_upscale_round) != 1){
+				fprintf(stderr, "Invalid value for --frame-count: '%s'\n", optarg);
+				print_help(stderr, argc, argv);
+				exit(1);
+			}	
+			break;
+		case 'w':
+			options.waifu2x_folder = optarg;
+			break;
+		case 'm':
+			options.waifu2x_model = optarg;
+			break;
+		case 'd':
+			options.dry_run = 1;
+			break;
+		case '?':
+			print_help(stderr, argc, argv);
+			exit(1);
+			break;
+		}
 	}
+	if (argc - optind < 2){
+		fprintf(stderr, "Not enough arguments for input/output!\n");
+		print_help(stderr, argc, argv);
+		exit(1);
+	}else if (argc - optind > 2){
+		fprintf(stderr, "Too many arguments for input/output!\n");
+		print_help(stderr, argc, argv);
+		exit(1);
+	}else{
+		options.input_filepath = argv[optind++];
+		options.output_filepath = argv[optind++];
+	}
+}
 
-	char* input_filepath = argv[1];
-	char* output_filepath = argv[2];
+int main(int argc, char* argv[]){
+	get_options(argc, argv);
 
+	if (options.dry_run){
+		fprintf(stdout, "Dry Run:\n\
+Input: %s\n\
+Output: %s\n\n\
+Frames per upscale batch: %zu\n\
+Waifu2x folder: %s\n\
+Waifu2x file: %s\n\
+Waifu2x model: %s\n",
+				options.input_filepath,
+				options.output_filepath,
+				options.frames_per_upscale_round,
+				options.waifu2x_folder,
+				options.waifu2x_file,
+				options.waifu2x_model);
+		exit(0);
+	}
+	
 	/* 
 	   Get the framerate of the video
 	*/
 	char framerate_str[10] = { '\0' };
-	get_framerate(input_filepath, framerate_str, 10);
+	get_framerate(options.input_filepath, framerate_str, 10);
 	fprintf(stdout, "Framerate: %s\n", framerate_str);
 	if (strlen(framerate_str) == 0) exit(1);
 
@@ -115,14 +224,21 @@ int main(int argc, char* argv[]){
 	*/
 	pipe_data ffmpeg_source_input_pipe = create_pipe_data();
 	pipe_data ffmpeg_source_output_pipe = create_pipe_data();
+
+	// Use a filter to make sure ffmpeg outputs a frame for every (1/fps) second, uniformly
+	// Use the framerate of the original video
+	const char* ffmpeg_filter_command_format = "fps=%s";
+	const size_t ffmpeg_filter_command_length = strlen(ffmpeg_filter_command_format) + strlen(framerate_str) + 1;
+	char* ffmpeg_filter_command = calloc(ffmpeg_filter_command_length, sizeof(char));
+	snprintf(ffmpeg_filter_command, ffmpeg_filter_command_length, ffmpeg_filter_command_format, framerate_str);
 	char* ffmpeg_source_command[] = { "ffmpeg", "-y",
-									  "-i", input_filepath,
-									  "-vf", "fps=30",
-									  //"-ss", "00:00:01",
+									  "-i", options.input_filepath,
+									  "-vf", ffmpeg_filter_command,
 									  "-vcodec", "png", "-f", "image2pipe", "-",
 									  NULL };
-	pid_t ffmpeg_source_pid = run_command(ffmpeg_source_command, NULL, &ffmpeg_source_input_pipe, &ffmpeg_source_output_pipe, 0);
+	pid_t ffmpeg_source_pid = run_command(ffmpeg_source_command, NULL, &ffmpeg_source_input_pipe, &ffmpeg_source_output_pipe, 1);
 	atomic_store(&session_data.ffmpeg_src_process, ffmpeg_source_pid);
+	free(ffmpeg_filter_command);
 	
 	dup2(dev_null_read, ffmpeg_source_input_pipe.files.write_to); // Send /dev/null to the input so that it doesn't use the terminal
 	pipe_data_close_read_from(&ffmpeg_source_input_pipe); // We shouldn't be able to read from the input
@@ -134,14 +250,13 @@ int main(int argc, char* argv[]){
 	*/
 	pipe_data ffmpeg_result_input_pipe = create_pipe_data();
 	//pipe_data ffmpeg_result_output_pipe = create_pipe_data();
+
 	char* ffmpeg_result_command[] = { "ffmpeg", "-y",
-									  "-i", input_filepath,
-									  //"-vcodec", "copy",
-									  //"-vsync", "drop",
-									  "-r", "30",
+									  "-i", options.input_filepath,
+									  "-r", framerate_str,
 									  "-vcodec", "png", "-f", "image2pipe", "-i", "-",
-									  /*"-c:a", "copy",*/ "-map", "1:v:0", "-map", "0:a:0", 
-									  output_filepath,
+									  "-map", "1:v:0", "-map", "0:a:0", 
+									  options.output_filepath,
 									  NULL };
 	pid_t ffmpeg_result_pid = run_command(ffmpeg_result_command, NULL, &ffmpeg_result_input_pipe, NULL, 1);//&ffmpeg_result_output_pipe);
 	atomic_store(&session_data.ffmpeg_rst_process, ffmpeg_result_pid);
@@ -163,7 +278,7 @@ int main(int argc, char* argv[]){
 	// Set up the temp files AFTER the interrupt handler is set
 	// If someone Ctrl-C's before this, we'll die right away
 	// If someone Ctrl-C's after this, we'll get rid of the tempfiles properly
-	session_data.temp_frame_count = FRAMES_PER_UPSCALE_ROUND;
+	session_data.temp_frame_count = options.frames_per_upscale_round;
 	session_data.temp_frames = calloc(session_data.temp_frame_count, sizeof(temp_frame));
 	{
 		int i;
@@ -172,14 +287,14 @@ int main(int argc, char* argv[]){
 	atexit(cleanup);
 	
     // waifu2x doesn't like using stdout
-	char* waifu2x_command[] = { "th", "./waifu2x.lua",
+	char* waifu2x_command[] = { "th", options.waifu2x_file,
 								"-force_cudnn", "1",
+								"-model_dir", options.waifu2x_model,
 								"-m", "noise_scale", "-noise_level", "1",
 								"-l", "/dev/stdin",
 								"-o", session_data.temp_frames[0].generic_output_filename, // TODO: Only do one calculation for generic_output_filename
 								NULL };
-	const size_t waifu2x_command_output_file_index = 7; 
-	char* waifu2x_location = "./waifu2x/";
+	//const size_t waifu2x_command_output_file_index = 7; 
 	waifu2x_process_data waifu2x_process;
 	
 	int current_frame = 0;
@@ -210,7 +325,7 @@ int main(int argc, char* argv[]){
 		waifu2x_process.input_pipe = create_pipe_data();
 		FILE* waifu2x_input_file = fdopen(waifu2x_process.input_pipe.files.write_to, "wb");
 		
-		waifu2x_process.pid = run_command(waifu2x_command, waifu2x_location, &waifu2x_process.input_pipe, NULL, 0);
+		waifu2x_process.pid = run_command(waifu2x_command, options.waifu2x_folder, &waifu2x_process.input_pipe, NULL, 0);
 		atomic_store(&session_data.waifu2x_process, waifu2x_process.pid);
 		pipe_data_close_read_from(&waifu2x_process.input_pipe);
 		
